7814ec2d187907e0e6d39aacc6e063e2bb5a1d08
nrlhlsaa <nurulhalisa2520@gmail.com>
2026-09-18 18:48:38 +0700

    Update AdminUserSeeder.php

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -12,7 +12,7 @@ class AdminUserSeeder extends Seeder
     {
         // Buat user admin
         $admin = User::firstOrCreate([
-            'email' => 'AdminBriliant@gmail.com',
+            'email' => 'AdminBrilliant@gmail.com',
         ], [
             'name' => 'admin',
             'password' => '$2y$12$BdkciryJxhZGxgVDyG91.esYrE2v80NmSvifm9F51zZWKqOmVs25K', // password sudah di-hash
